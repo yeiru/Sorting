0909b3760a770f8b0944b5f9d4888bef78f099bf
Luis Eduardo Montes Siordia <eduardo.montes@redudg.udg.mx>
2015-08-24 09:18:57 -0500

    Proceso de implementacion de quicksort

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -42,7 +42,32 @@ public class Sorting {
 	}
 
 	public static int[] quickSort(int[] array) {
-
+		int length = array.length;
+		if(length == 1)
+			return array;
+		int pivot = (length-1)/2;
+		int value = array[pivot];
+		int aux;
+		int i = 0, j = length-1;
+		while(i < j) {
+			while(i < pivot) {
+				if(array[i] >= value) {
+					break;
+				}
+				++i;
+			}
+			while(j > pivot) {
+				if(array[j] < value) {
+					break;
+				}
+				--j;
+			}
+			aux = array[j];
+			array[j] = array[i];
+			array[i] = aux;
+		}
+		quickSort(Array.copyOfRange(array, 0, pivot-1));
+		quickSort(Array.copyOfRange(array, pivot+1, length-1));
 		return array;
 	}
 
